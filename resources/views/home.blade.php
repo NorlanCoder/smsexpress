22cc9878e101aa4832f53919980d03bd082dd26a
NorlanCoder <fabien.amoussou20@gmail.com>
2023-08-03 00:19:18 +0200

    Frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="shortcut icon" href="{{asset('image/logo.jpg')}}" type="image/x-icon">
     <title>SMS EXPRESS</title>
     {{-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> --}}
     @vite('resources/css/app.css')
@@ -13,18 +14,18 @@
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <nav class="relative z-50 flex justify-between">
           <div class="flex items-center md:gap-x-12">
-            <a aria-label="Home" href="#">
-              <svg aria-hidden="true" viewBox="0 0 109 40" class="h-10 w-auto"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 20c0 11.046 8.954 20 20 20s20-8.954 20-20S31.046 0 20 0 0 8.954 0 20Zm20 16c-7.264 0-13.321-5.163-14.704-12.02C4.97 22.358 6.343 21 8 21h24c1.657 0 3.031 1.357 2.704 2.98C33.32 30.838 27.264 36 20 36Z" fill="#2563EB"></path><path d="M55.528 26.57V15.842H52V13.97h9.108v1.872h-3.636V26.57h-1.944Z" fill="#0F172A"></path><path d="M83.084 26.57v-12.6h5.346c.744 0 1.416.18 2.016.54a3.773 3.773 0 0 1 1.44 1.44c.36.612.54 1.302.54 2.07 0 .78-.18 1.482-.54 2.106a4 4 0 0 1-1.44 1.494c-.6.36-1.272.54-2.016.54h-2.646v4.41h-2.7Zm2.664-6.84h2.376c.288 0 .546-.072.774-.216.228-.156.408-.36.54-.612a1.71 1.71 0 0 0 .216-.864c0-.324-.072-.606-.216-.846a1.394 1.394 0 0 0-.54-.576 1.419 1.419 0 0 0-.774-.216h-2.376v3.33ZM106.227 26.57V13.25h2.556v13.32h-2.556Z" fill="#2563EB"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M95.906 26.102c.636.432 1.35.648 2.142.648.444 0 .864-.066 1.26-.198a4.25 4.25 0 0 0 1.062-.558 3.78 3.78 0 0 0 .702-.668v1.244h2.574v-9.522h-2.538v1.248a3.562 3.562 0 0 0-.648-.672 3.13 3.13 0 0 0-1.026-.558 3.615 3.615 0 0 0-1.278-.216c-.828 0-1.566.216-2.214.648-.648.42-1.164 1.002-1.548 1.746-.372.732-.558 1.578-.558 2.538 0 .96.186 1.812.558 2.556.372.744.876 1.332 1.512 1.764Zm4.104-1.908c-.36.228-.78.342-1.26.342-.468 0-.882-.114-1.242-.342a2.387 2.387 0 0 1-.828-.954c-.204-.42-.306-.906-.306-1.458 0-.54.102-1.014.306-1.422.204-.408.48-.726.828-.954.36-.24.774-.36 1.242-.36.48 0 .9.12 1.26.36.36.228.636.546.828.954.204.408.306.882.306 1.422 0 .552-.102 1.038-.306 1.458a2.218 2.218 0 0 1-.828.954Z" fill="#2563EB"></path><path fill-rule="evenodd" clip-rule="evenodd" d="m76.322 23.197 2.595 3.373h2.268l-3.662-4.787 3.338-4.663h-2.196l-2.162 3.334-2.554-3.334h-2.34l3.652 4.71-3.634 4.74h2.196l2.5-3.373ZM62.738 26.102a3.78 3.78 0 0 0 2.142.648c.456 0 .888-.072 1.296-.216.42-.144.798-.336 1.134-.576a3.418 3.418 0 0 0 .864-.835v1.447h1.872v-9.45h-1.872v1.45a3.118 3.118 0 0 0-.72-.82 3.2 3.2 0 0 0-1.062-.612 4.033 4.033 0 0 0-1.35-.216c-.828 0-1.578.21-2.25.63-.66.42-1.188 1.002-1.584 1.746-.384.732-.576 1.572-.576 2.52 0 .936.192 1.776.576 2.52.384.744.894 1.332 1.53 1.764Zm4.122-1.476c-.432.276-.93.414-1.494.414a2.682 2.682 0 0 1-1.476-.414 2.987 2.987 0 0 1-1.008-1.134c-.24-.492-.36-1.05-.36-1.674 0-.612.12-1.158.36-1.638.252-.48.588-.858 1.008-1.134a2.682 2.682 0 0 1 1.476-.414c.564 0 1.062.138 1.494.414.432.276.768.654 1.008 1.134.252.48.378 1.026.378 1.638 0 .624-.126 1.182-.378 1.674-.24.48-.576.858-1.008 1.134Z" fill="#0F172A"></path></svg>
+            <a aria-label="Home" href="#presentation">
+              <img src="{{asset('image/logo.jpg')}}" width="50" height="50" alt="">
             </a>
             <div class="hidden md:flex md:gap-x-6">
-              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#features">Nos chiffres</a>
-              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#testimonials">Pricing</a>
-              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">Contact</a>
+              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#nos-chiffres">Nos chiffres</a>
+              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">Pricing</a>
+              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#contact">Contact</a>
             </div>
           </div>
           <div class="flex items-center gap-x-5 md:gap-x-8">
             <div class="hidden md:block">
-              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="/login">FAQ</a>
+              <a class="inline-block rounded-lg px-2 py-1 text-md text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#faq">FAQ</a>
             </div>
             <a class="group inline-flex items-center justify-center rounded py-2 px-4 text-md font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-teal-500 text-white hover:text-slate-100 hover:bg-teal-600 active:bg-teal-800 active:text-teal-100 focus-visible:outline-teal-600" href="/register">
               <span>Mon compte</span>
@@ -41,7 +42,7 @@
       </div>
 	</header>
     <main>
-		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32" id="presentation">
 			<h1 class="mx-auto max-w-4xl font-display text-5xl font-bold tracking-tight text-slate-900 sm:text-7xl">Engagez votre<!-- --> 
 			<span class="relative whitespace-nowrap text-teal-400">
 				<svg aria-hidden="true" viewBox="0 0 418 42" class="absolute left-0 top-2/3 h-[0.58em] w-full fill-teal-600/50" preserveAspectRatio="none"><path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path></svg>
@@ -74,7 +75,7 @@
 		</div>
 
 		{{-- Stat D'utilisation --}}
-		<div class="bg-slate-100/30 py-24 sm:py-32">
+		<div class="bg-slate-100/30 py-24 sm:py-32" id="nos-chiffres">
 			<div class="mx-auto max-w-7xl px-6 lg:px-8">
 			<div class="mx-auto max-w-2xl lg:max-w-none">
 				<div class="text-center">
@@ -181,12 +182,12 @@
 		</section>
 
 		{{-- Contact --}}
-		<section id="get-started-today" class="relative overflow-hidden bg-slate-100 py-32">
+		<section id="contact" class="relative overflow-hidden bg-slate-100 py-32">
 			<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative">
 				<div class="mx-auto max-w-3xl text-center">
 					<h2 class="font-display text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">Contactez-nous</h2>
 					<p class="mt-4 text-lg tracking-tight text-black">Nous sommes ravis de vous entendre ! Si vous avez des questions, des commentaires ou que vous souhaitez simplement discuter, n'hésitez pas à nous contacter en utilisant le formulaire ci-dessous. Nous ferons de notre mieux pour vous répondre dans les plus brefs délais.</p>
-					<a class="group inline-flex items-center justify-center rounded py-2 px-4 text-md font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-800 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10" href="/register">Get 6 months free</a>
+					<a class="group inline-flex items-center justify-center rounded py-2 px-4 text-md font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-800 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-10" href="mailto:fabienamoussou20062001@gmail.com">Envoyez nous un e-mail</a>
 				</div>
 			</div>
 		</section>
@@ -203,48 +204,24 @@
 					<li>
 						<ul role="list" class="flex flex-col gap-y-8">
 							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">Does TaxPal handle VAT?</h3>
-								<p class="mt-4 text-sm text-slate-700">Well no, but if you move your company offshore you can probably ignore it.</p>
+								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">Quels sont les avantages de SMS Express par rapport à d'autres fournisseurs ?</h3>
+								<p class="mt-4 text-sm text-slate-700">SMS Express offre une interface conviviale, une livraison rapide des SMS, une gestion efficace des contacts et des options de personnalisation pour vos messages.</p>
 							</li>
+						</ul>
+					</li>
+					<li>
+						<ul role="list" class="flex flex-col gap-y-8">
 							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">Can I pay for my subscription via purchase order?</h3>
-								<p class="mt-4 text-sm text-slate-700">Absolutely, we are happy to take your money in all forms.</p>
+								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">Comment puis-je envoyer des SMS en masse avec SMS Express ?</h3>
+								<p class="mt-4 text-sm text-slate-700">Connectez-vous à votre compte SMS Express, importez vos contacts, rédigez votre message et choisissez les destinataires. Ensuite, cliquez sur "Envoyer" pour lancer la diffusion des SMS.</p>
 							</li>
+						</ul>
+					</li>
+					<li>
+						<ul role="list" class="flex flex-col gap-y-8">
 							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">How do I apply for a job at TaxPal?</h3>
+								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">SMS Express garantit-il la confidentialité de mes données ?</h3>
 								<p class="mt-4 text-sm text-slate-700">We only hire our customers, so subscribe for a minimum of 6 months and then let’s talk.</p>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<ul role="list" class="flex flex-col gap-y-8">
-							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">What was that testimonial about tax fraud all about?</h3>
-								<p class="mt-4 text-sm text-slate-700">TaxPal is just a software application, ultimately your books are your responsibility.</p>
-							</li>
-							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">TaxPal sounds horrible but why do I still feel compelled to purchase?</h3>
-								<p class="mt-4 text-sm text-slate-700">This is the power of excellent visual design. You just can’t resist it, no matter how poorly it actually functions.</p>
-							</li>
-							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">I found other companies called TaxPal, are you sure you can use this name?</h3>
-								<p class="mt-4 text-sm text-slate-700">Honestly not sure at all. We haven’t actually incorporated or anything, we just thought it sounded cool and made this website.</p>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<ul role="list" class="flex flex-col gap-y-8">
-							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">How do you generate reports?</h3>
-								<p class="mt-4 text-sm text-slate-700">You just tell us what data you need a report for, and we get our kids to create beautiful charts for you using only the finest crayons.</p>
-							</li>
-							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">Can we expect more inventory features?</h3>
-								<p class="mt-4 text-sm text-slate-700">In life it’s really better to never expect anything at all.</p>
-							</li>
-							<li>
-								<h3 class="font-display font-semibold text-lg leading-7 text-slate-900">I lost my password, how do I get into my account?</h3>
-								<p class="mt-4 text-sm text-slate-700">Send us an email and we will send you a copy of our latest password spreadsheet so you can find your information.</p>
 							</li>
 						</ul>
 					</li>
@@ -256,13 +233,13 @@
 		<footer class="bg-slate-50">
 			<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<div class="py-16">
-					<p class="text-center text-2xl font-semibold"><a href="" class="text-teal-400">SMSExpress</a></p>
+					<p class="text-center flex justify-center text-2xl font-semibold"><a href="#presentation" class="text-teal-400 text-center"><img src="{{asset('image/logo.jpg')}}" width="50" height="50" alt=""></a></p>
 					<nav class="mt-10 text-sm" aria-label="quick links">
 						<div class="-my-1 flex justify-center gap-x-6">
-							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#features">Nos chiffres</a>
-							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#testimonials">Pricing</a>
-							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#testimonials">FAQ</a>
-							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">Contact</a>
+							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#nos-chiffres">Nos chiffres</a>
+							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#pricing">Pricing</a>
+							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#faq">FAQ</a>
+							<a class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900" href="#contact">Contact</a>
 						</div>
 					</nav>
 				</div>
